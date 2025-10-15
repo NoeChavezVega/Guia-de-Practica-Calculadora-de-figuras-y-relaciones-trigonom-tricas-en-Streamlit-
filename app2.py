de5c886fc002084fe499a917b64c8242b1245483
NoeChavezVega <a377347@uach.mx>
2025-10-15 08:44:37 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,4 +2,4 @@ import streamlit as st
 import math 
 st.title("Calculadora de figuras y Relaciones trigonometricas en Streamlit")
 st.title("Figuras geometricas")
-figura = st.selesctbox("Seleccionar figura",["Cuadrado","Rectangulo","Triangulo","Circulo"])
+figura = st.selectbox("Seleccionar figura",["Cuadrado","Rectangulo","Triangulo","Circulo"])
