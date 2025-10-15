b1f4fe6ea4614891c2822969a188b75682e130f9
NoeChavezVega <a377347@uach.mx>
2025-10-15 08:40:09 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,8 +2,7 @@ import streamlit as st
 import math 
 st.title("Calculadora de figuras y Relaciones trigonometricas en Streamlit")
 st.title("Figuras geometricas")
-figura = st.selesctbox("Seleccionar figura")
+figura = st.selesctbox("Seleccionar figura",["Cuadrado","Rectangulo","Triangulo","Circulo"])
 radio = st.slider("selecciona el radio", 0.0, 10.0, 5.0)
 area = math.pi * radio**2
 st.write(f"El area del circulo con radio {radio} es:{area:.2f}")
-perimetro = 
