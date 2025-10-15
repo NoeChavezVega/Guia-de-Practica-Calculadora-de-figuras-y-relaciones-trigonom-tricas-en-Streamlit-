20b2a6c6b41ee5a94b6f10db3626fbe73bf57b32
NoeChavezVega <a377347@uach.mx>
2025-10-15 08:48:21 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,3 +8,4 @@ Perimetro = None
 if figura == "Cuadrado":
   radio = st.slider("selecciona el radio", 0.0, 10.0, 5.0)
 area = math.pi * radio**2
+st.write(f"El area del circulo con radio {radio} es:{area:.2f}")
