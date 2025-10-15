a9b7052fa1a3e9f2fa06866b797594a3e0c3a6e7
NoeChavezVega <a377347@uach.mx>
2025-10-15 09:22:38 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -16,12 +16,9 @@ elif figura == "Triangulo":
   lado_a = st.number_input("selecciona lado_a", 0.0, 10.0, 5.0)
   lado_b = st.number_input("selecciona lado_b", 0.0, 10.0, 5.0)
   area = 0.5 * base * altura
-elif figura == "Cuadrado":
-  lado = st.number_input("Selecciona el lado", 0.0, 10.0, 5.0)
+   st.write(f"El area del triangulo con base {base} es:{area:.2f}")
+ 
 
-st.write(f"El area del Cuadrado con lado {lado} es:{area:.2f}")
-perimetro = 4*lado
-st.write(f"El perimetro del Cuadrado con lado {lado} es:{perimetro:.2f}")
 
 
 
