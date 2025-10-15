1f31c27fe3f388cd1223d772f9537ac680013279
NoeChavezVega <a377347@uach.mx>
2025-10-15 09:06:14 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,6 +3,10 @@ import math
 st.title("Calculadora de figuras y Relaciones trigonometricas en Streamlit")
 st.title("Figuras geometricas")
 figura = st.selectbox("Seleccionar figura",["Cuadrado","Rectangulo","Triangulo","Circulo"])
+if figura == "Circulo":
+  radio = st.slider("selecciona el radio", 0.0, 10.0, 5.0)
+  area = math.pi * radio**2
+  st.write(f"El area del circulo con radio {radio} es:{area:.2f}")
 if figura == "Cuadrado":
   lado = st.slider("Selecciona el lado", 0.0, 10.0, 5.0)
 area = lado**2
